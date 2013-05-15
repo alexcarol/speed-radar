166b53b04def8feeca3da744875092e4f01fa38d
Alex Carol <alex.carol.c@gmail.com>
2013-05-16 00:00:26 +0200

    Created RadarList class, needs a paint method or something

---
+++ core/src/main/java/com/nya/speedradar/core/RadarList.java
@@ -0,0 +1,46 @@
+package com.nya.speedradar.core;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class RadarList {
+
+  private ArrayList<Pole> poles;
+  private int currentPole = 0;
+
+  public RadarList(long roadLength, int numPoles, int numRadars) {
+    poles = new ArrayList<Pole>(numPoles);
+
+
+    long roadSectionLength = roadLength/numPoles;
+
+    for (int i = 0; i < numPoles; ++i) {
+      //if we wanted we could make pole random in each section
+      Pole p = new Pole(roadSectionLength/2 + roadSectionLength*i);
+    }
+
+    Random rand = new Random(System.currentTimeMillis());
+
+    for (int i = 0; i < numRadars; ++i) {
+      poles.get(rand.nextInt(numRadars));
+    }
+  }
+
+  public boolean isPolePassed() {
+    return
+  }
+
+  public ArrayList<Pole> getPoles()
+  {
+    return poles;
+  }
+}
+
+class Pole {
+  public long positionY;
+  public boolean radar = false;
+
+  public Pole(long pos) {
+    positionY = pos;
+  }
+}
