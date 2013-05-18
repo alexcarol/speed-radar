92c37e7f713580ed2381702c4ff293a464948f18
Alex Carol <alex.carol.c@gmail.com>
2013-05-18 18:33:25 +0200

    Fixed radar

--- core/src/main/java/com/nya/speedradar/core/RadarList.java
+++ core/src/main/java/com/nya/speedradar/core/RadarList.java
@@ -22,7 +22,7 @@ public class RadarList {
     Random rand = new Random(System.currentTimeMillis());
 
     for (int i = 0; i < numRadars; ++i) {
-      poles.get(rand.nextInt(numRadars));
+      poles.get(rand.nextInt(numRadars)).radar = true;
     }
   }
 
